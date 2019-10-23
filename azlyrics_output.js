6a4d8da5de9d556918c8bf52de59079da130947a
Kilia <kiliarl@hotmail.com>
2019-10-23 14:01:28 +0200

    update

---
+++ azlyrics_output.js
@@ -0,0 +1,166 @@
+var azlyrics_output = 
+    
+    
+    
+    
+    
+    AZLyrics - Search: booty/?__a=1
+
+    
+    
+
+    
+    
+
+    
+    
+     
+    
+
+
+
+
+
+    
+        
+            
+                
+            
+            
+                
+            
+            
+        
+        
+            
+                
+                    A
+                    B
+                    C
+                    D
+                    E
+                    F
+                    G
+                    H
+                    I
+                    J
+                    K
+                    L
+                    M
+                    N
+                    O
+                    P
+                    Q
+                    R
+                    S
+                    T
+                    U
+                    V
+                    W
+                    X
+                    Y
+                    Z
+                    #
+                
+            
+        
+
+        
+
+            
+                
+                    
+                    
+            	   Search
+          	
+                
+            
+
+        
+    
+
+
+
+
+    
+        
+            
+                
+            
+        
+    
+
+
+
+    
+        
+            
+            Sorry, your search returned no results. Try to compose less restrictive search query or check spelling.
+            
+            
+                
+                    
+                    
+            	   Search
+          	
+                
+            
+        
+
+
+
+    
+        
+            Submit Lyrics
+            Soundtracks
+            Facebook
+            Contact Us
+        
+    
+
+
+
+
+    
+        
+            
+                
+            
+        
+    
+
+
+
+
+    
+        
+            Advertise Here
+            Privacy Policy
+            Cookie Policy
+            DMCA Policy
+        
+    
+
+
+    
+        
+            All lyrics are property and copyright of their owners. All lyrics provided for educational purposes and personal use only.
+            
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
