116aba654109dcb60999b2c9914c5f3223bd8590
Kilia <kiliarl@hotmail.com>
2019-10-23 17:17:25 +0200

    update

--- azlyrics.js
+++ azlyrics.js
@@ -7,7 +7,7 @@ var spawn = require('child_process').spawn;
 var fs = require('fs');
 var url = require('url');
 
-var port = 2100;
+var port = 3000;
 var app = express();
 
 var DOWNLOAD_DIR = './';
