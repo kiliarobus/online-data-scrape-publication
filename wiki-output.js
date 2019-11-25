07749eeb24464ebdcff99e4aa82833f91b2a679d
Kilia <kiliarl@hotmail.com>
2019-11-25 09:45:04 +0100

    update

--- wiki-output.js
+++
@@ -1,5 +0,0 @@
-{
-    "articleTitle": "Phyllotaxis",
-    "articleImg": "//upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Aloe_polyphylla_1.jpg/275px-Aloe_polyphylla_1.jpg",
-    "articleParagraph": "The term was coined by Charles Bonnet to describe the arrangement of leaves on a plant.[2]\n"
-}
